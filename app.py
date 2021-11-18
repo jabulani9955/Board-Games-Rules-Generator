1cac798db4d93c541d1dbcfdeaba3be8e7800ea8
jabulani9955 <denisnovikov1997@gmail.com>
2021-11-18 15:34:40 +0300

    fix

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask, render_template, request
 import random
-
+from model import generate
+import textwrap
 
 app = Flask(__name__)
 
+
+# @app.route('/home')
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('prediction.html')
+
+
+
+@app.route('/results', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        prompt = request.form.get('input')
+        result = textwrap(generate(prompt, len_gen=100, temperature=.8))
+    return render_template('prediction.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run()
