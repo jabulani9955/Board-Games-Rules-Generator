3500e568d8ac4a274c3c0677d2a1ed58f7510aba
jabulani9955 <denisnovikov1997@gmail.com>
2021-11-20 20:06:45 +0300

    moved

--- text_processing.py
+++
@@ -1,5 +0,0 @@
-def processing(input_string: str):
-    output_string = ' '.join(input_string.split()).replace('»', '', 1).strip().capitalize()
-    while not (output_string.endswith('.') or output_string.endswith('!')):
-        output_string = output_string[:-1]
-    return output_string
